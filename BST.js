fd48b17a8c90d4a70f4ce3c393cd642492fe3c7c
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-15 18:08:45 -0500

    Write node and tree factory functions
    node will be a single node in the tree and
    tree will contain the root node that is the
    return value of the buildTree function.

--- BST.js
+++ BST.js
@@ -1,2 +1,15 @@
 'use strict';
 
+function node(value) {
+    let data = value;
+    let left = null;
+    let right = null;
+
+    return { data, left, right };
+};
+
+function tree(arr) {
+    let root = buildTree(arr);
+
+    return { root };
+};
