6d78461002ff00ac5c2ab8f15f61106f53a01a58
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-16 00:09:39 -0500

    Write deleteNode function
    deletes a given node in the tree

--- BST.js
+++ BST.js
@@ -49,7 +49,64 @@ function insert(value, root) {
     return root;
 };
 
+function deleteNode(value, root) {
+    if (root === null) { return root };
+
+    //recursively searching for node to delete
+    if (value < root.data) {
+        root.left = deleteNode(value, root.left);
+        return root;
+    } else if (value > root.data) {
+        root.right = deleteNode(value, root.right);
+        return root;
+    };
+
+    // --- at this point root = node to delete --- //
+
+    // if node to delete has no children
+    if (root.left === null && root.right === null) {
+        root = null;
+        return root;
+    } else {
+        let parent = root;
+        let child = root.right;
+
+        //get left most leaf of right child of node to delete
+        while (child.left !== null) {
+            parent = child;
+            child = child.left;
+        };
+
+        //get rid of the node thats replacing the root
+        if (child.data < parent.data) {
+            parent.left = null;
+        } else {
+            parent.right = null;
+        };
+
+        //replace the root with value of left most leaf of right child
+        root.data = child.data;
+
+        return root;
+    };
+};
+
 let test = tree([1,2,3,4,5,6,7]);
 
 console.log(insert(0, test));
 
+console.log(deleteNode(4, test));
+
+/*
+deleteNode(2, 4)
+    deleteNode(2, 2)
+        parent = 2
+        child = 3
+
+        parent.right(3) = null
+
+        root = 3
+
+        return 2;
+    return 4;
+*/
