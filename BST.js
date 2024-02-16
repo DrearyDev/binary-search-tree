d4abf338138eff6c3218472304e89949b6944984
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-15 20:55:05 -0500

    Make buildTree function a method on tree factory
    since the tree factory function will always use it
    to set the root value and then we can use the root
    in other methods like insert() and delete().

--- BST.js
+++ BST.js
@@ -11,28 +11,29 @@ function node(value) {
 };
 
 function tree(arr) {
+    const buildTree = (arr) => {
+        let array = mergeSort(arr);//sort the array
+        array = [...new Set(array)];//remove duplicate values
+
+        let start = 0;
+        let end = array.length - 1;
+        let mid = Math.floor((array.length) / 2);
+
+        if (start > end) { return null };
+
+        let root = node(array[mid]);
+        root.left = buildTree([...array].splice(start, mid));//non-inclusive so mid isnt apart of array
+        root.right = buildTree([...array].splice(mid+1, end));//add one to avoid including mid
+
+        return root;
+    };
     let root = buildTree(arr);
+    
 
-    return { root };
-};
-
-function buildTree(arr) {
-    let array = mergeSort(arr);//sort the array
-    array = [...new Set(array)];//remove duplicate values
-
-    let start = 0;
-    let end = array.length - 1;
-    let mid = Math.floor((array.length) / 2);
-
-    if (start > end) { return null };
-
-    let root = node(array[mid]);
-    root.left = buildTree([...array].splice(start, mid));//non-inclusive so mid isnt apart of array
-    root.right = buildTree([...array].splice(mid+1, end));//add one to avoid including mid
-
-    return root;
+    return { root, buildTree };
 };
 
 let test = tree([1,2,3,4,5,6,7]);
 
-console.log(test);
+console.log(test.root);
+
