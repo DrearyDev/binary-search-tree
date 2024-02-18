4492245694fd66cc3dc1e27534c1b335f7c498a0
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-17 20:27:47 -0500

    Prove that everything works in driver.js
    logging out the tests that the Odin Project gave for me
    to test my script.

--- driver.js
+++ driver.js
@@ -11,3 +11,105 @@ let keys = new Array(7);
 for (let i = 0; i < keys.length; i++) {
     keys[i] = randomNumbers();
 };
+
+
+// --- building tree --- //
+let test = tree(keys);
+
+console.log('Current root of the tree:');
+console.log(test.getRoot());
+
+// --- checking if tree is balanced --- //
+
+console.log('is tree balanced:');
+console.log(test.isBalanced());// true
+
+// --- printing all elements in 'level-Order' --- //
+console.log('--------------------------Level Order:');
+
+test.levelOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'pre-order' --- //
+console.log('--------------------------Pre-Order:');
+
+test.preOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'in-order' --- //
+console.log('--------------------------In-Order:');
+
+test.inOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'post-order' --- //
+console.log('--------------------------Post-Order:');
+
+test.postOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- unbalanceing the tree by inserting more numbers --- //
+console.log('--------------------------Unbalancing the tree:');
+
+for (let i = 0; i < keys.length; i++) {
+    test.insertNode(randomNumbers(10000));
+};
+
+console.log('is tree balanced:');
+console.log(test.isBalanced());//false
+
+// --- rebalancing the tree --- //
+console.log('--------------------------Rebalancing the tree:');
+
+test.reBalance();
+
+console.log('is tree balanced:');
+console.log(test.isBalanced());//true
+
+// --- printing all elements in 'level-Order' --- //
+console.log('--------------------------Level Order:');
+
+test.levelOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'pre-order' --- //
+console.log('--------------------------Pre-Order:');
+
+test.preOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'in-order' --- //
+console.log('--------------------------In-Order:');
+
+test.inOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
+
+// --- printing all elements in 'post-order' --- //
+console.log('--------------------------Post-Order:');
+
+test.postOrder(function(node){
+    console.log(node);
+});
+
+console.log('--------------------------');
